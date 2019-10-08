131e6aa2ffffe1b48732cc2b9e45018c295ab16c
Kkmilo <kamiilo139698@gmail.com>
2019-10-08 16:16:07 -0500

    second-Comment

--- Assets/Script/Mapeo.cs
+++ Assets/Script/Mapeo.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class Mapeo : MonoBehaviour
 {
-    public Material[] skins= new Material [8];
-    public List<Material>myList; 
+    public List<Material>myList = new List<Material>(16);
+    public GameObject fichas;
+    public  Material temp;
 
     void Start()
     {
-        
+        Shuffle(myList);
+        for (int i = 0; i < 16; i++)
+        {
+            foreach (var item in myList)
+            {
+                int index=0;
+                temp = myList[index];
+                index++;
+            }
+            fichas.transform.GetChild(i).GetChild(0).GetComponent<MeshCollider>().material = temp;
+            myList.Remove(temp);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void Shuffle(List<Material> list)
+    {
+        int n = list.Count;
+        while(n>1)
+        {
+            int k = Random.Range(0, n + 1);
+            Material valu = list[k];
+            list[k] = list[n];
+            list[n] = valu;
+
+        }
     }
 }
