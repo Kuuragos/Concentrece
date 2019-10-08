45cb3e9de48bf5fc6149d1884d3d435c0edcd0e6
Kkmilo <kamiilo139698@gmail.com>
2019-10-08 13:21:29 -0500

    comit_1

---
+++ Assets/Script/Mapeo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mapeo : MonoBehaviour
+{
+    public Material[] skins= new Material [8];
+    public List<Material>myList; 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Rotar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotar : MonoBehaviour
+{
+    public bool show = false;
+    private void OnMouseDown()
+    {
+        show = true;
+            Invoke("Hide",2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (show)
+        {
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.up * 180, 2 * Time.deltaTime));
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.zero, 2 * Time.deltaTime));
+
+        }
+    }
+    public void Hide()
+    {
+        show = false;
+    }
+}
